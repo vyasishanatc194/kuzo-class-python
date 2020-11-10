7a703473dcb8551380ce9b0755a1fb864095d9d6
fauzan.citrusbug <fauzan.citrusbug@gmail.com>
2020-11-10 18:48:29 +0530

    updated code

--- core/api/views/influencer_details.py
+++ core/api/views/influencer_details.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework import generics
 
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from core.models import UserProfile
+from core.models import UserProfile, Event
 from core.api.apiviews import MyAPIView
 from core.api.serializers import InfluencerDetailsListSerializer
 
@@ -23,10 +23,17 @@ class InfluencerDetailsListAPIView(MyAPIView):
     permission_classes = (AllowAny,)
     serializer_class = InfluencerDetailsListSerializer
 
-    def get(self, request,pk, format=None):    
-        influencer = UserProfile.objects.filter(user__id=pk)
-        serializer = self.serializer_class(influencer, many=True, context={"request": request})
-        return Response({"status": "OK", "message": "Successfully fetched influencer details ", "data": serializer.data})
+    def get(self, request, pk, format=None):  
+        try:
+            user_obj = Event.objects.filter(id=pk).first()
+            influencer = UserProfile.objects.filter(user__id=user_obj.user.id)
+            serializer = self.serializer_class(influencer, many=True, context={"request": request})
+
+            new_serializer_data = list(serializer.data)
+            new_serializer_data.append({'current_event_id': pk})
+
+            return Response({"status": "OK", "message": "Successfully fetched influencer details ", "data": new_serializer_data})
 
 
-    
+        except Event.DoesNotExist:
+            return Response({"status": "FAIL", "message": "Event not found", "data": []})
