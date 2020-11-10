afa84653ad1cd60e3b7b394b631068d74b70abf3
Ubuntu <ubuntu@ip-172-31-36-39.us-west-2.compute.internal>
2020-11-10 04:19:30 +0000

    updated code

---
+++ static/core/js/userroles.js
@@ -0,0 +1,517 @@
+/*global $ */
+'use strict';
+
+var userroles = {
+
+    // ------------------------------------------------------------------------
+    // Users
+    // ------------------------------------------------------------------------
+    users: {
+
+        index: function () {
+            $('#user-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },],
+
+            });
+
+        },
+
+        details: function () {
+            $('.groups-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available groups',
+                selectedListLabel: 'Chosen groups',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    // ------------------------------------------------------------------------
+    // Groups
+    // ------------------------------------------------------------------------
+    groups: {
+
+        index: function () {
+            $('#group-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+
+    // ------------------------------------------------------------------------
+
+    genre: {
+        index: function () {
+            $('#genre-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    // ------------------------------------------------------------------------
+
+    newoffers: {
+        index: function () {
+            $('#offer-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    // ----------------------------------------------------------------
+
+    banner: {
+        index: function () {
+            $('#banner-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    // ----------------------------------------------------------------
+
+    subscriptionplans: {
+        index: function () {
+            $('#subscriptionplan-table').DataTable({
+                pageLength: 10,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    //-------------------------------------------------------------------------
+    credit: {
+        index: function () {
+            $('#credit-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+    //------------------------------------------------------------------------
+
+    faq: {
+        index: function () {
+            $('#faq-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    //------------------------------------------------------------------------
+    creditorder: {
+        index: function () {
+            $('#creditorder-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+    //------------------------------------------------------------------------
+
+    subscriptionorder: {
+        index: function () {
+            $('#subscriptionorder-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+    //-------------------------------------------------------------------------
+   
+
+    //------------------------------------------------------------------------
+    
+    contactusTab: {
+        index: function () {
+            $('#contactus-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    //-------------------------------------------------------------------------
+    category: {
+        index: function () {
+            $('#category-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+
+    //-------------------------------------------------------------------------
+    banner: {
+        index: function () {
+            $('#banner-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+    // ------------------------------------------------------------------------
+
+    planorder: {
+        index: function () {
+            $('#planorder-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+    },
+
+    // -------------------------------------------------------------------
+
+    productcategory: {
+        index: function () {
+            $('#productcategories-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    // -------------------------------------------------------------------
+
+    events: {
+        index: function () {
+            $('#event-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    // ----------------------------------------------------------------------
+
+   
+
+    //---------------------------------------------------------------------
+
+   
+    // ----------------------------------------------------------------
+
+    eventorder: {
+        index: function () {
+            $('#eventorder-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    // ----------------------------------------------------------------
+
+    agendas: {
+
+        index: function () {
+            $('#agenda-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    //---------------------------------------------------------------------
+
+    eventpracticeaudienceqa: {
+        index: function () {
+            $('#eventpracticeaudienceqa-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    // -------------------------------------------------------------------
+
+    flashdeals: {
+        index: function () {
+            $('#flashdeal-table').DataTable({
+                pageLength: 25,
+                responsive: true,
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                },]
+            });
+        },
+
+        details: function () {
+            $('.permissions-select').bootstrapDualListbox({
+                nonSelectedListLabel: 'Available user permissions',
+                selectedListLabel: 'Chosen user permissions',
+                preserveSelectionOnMove: 'moved',
+                moveOnSelect: false
+            });
+        }
+
+    },
+
+    // -------------------------------------------------------------------
+
+};
