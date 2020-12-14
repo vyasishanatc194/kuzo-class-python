8f926da7ecff356deb64d101be6e2ae5c267ea03
fauzan2053 <fauzan2053@gmail.com>
2020-12-14 15:56:09 +0530

    Fiexd bug

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["44.225.113.133","localhost", "0.0.0.0",'127.0.0.1']
+ALLOWED_HOSTS = ["44.225.113.133","localhost", "0.0.0.0",'127.0.0.1','44.225.113.133:8000']
 
 # DATABASES
 # ------------------------------------------------------------------------------
