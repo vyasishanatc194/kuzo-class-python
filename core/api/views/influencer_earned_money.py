61ccbfbc2486856f4fa2c19fcbe8e59794c9d8e6
fauzan2053 <fauzan2053@gmail.com>
2021-01-04 21:35:20 +0530

    Modification in calculation

--- core/api/views/influencer_earned_money.py
+++ core/api/views/influencer_earned_money.py
@@ -5,12 +5,18 @@ from django.db.models import Sum
 import stripe
 
 from core.api.apiviews import MyAPIView
-from core.utils.daily_earning_money import daily_earning, monthly_earning, yearly_earning
+from core.utils.daily_earning_money import (
+    daily_earning,
+    monthly_earning,
+    yearly_earning,
+)
 
 from core.models import EventOrder
 
-current_datetime =  datetime.now()
+current_datetime = datetime.now()
 current_datetime += timedelta(days=1)
+today = datetime.now()
+
 
 # .................................................................................
 # InfluencerEarnMoneyListAPIView Plan API
@@ -30,22 +36,52 @@ class InfluencerEarnMoneyListAPIView(MyAPIView):
         try:
 
             data = {}
-            result =[]
-            day=0
+            result = []
+            day = 0
 
             if request.user.influencer_stripe_account_id:
-                new_result = stripe.Account.retrieve(str(request.user.influencer_stripe_account_id))
-                data['next_payout'] = new_result['settings']['payouts']
+                new_result = stripe.Account.retrieve(
+                    str(request.user.influencer_stripe_account_id)
+                )
+                data["next_payout"] = new_result["settings"]["payouts"]
 
             search = request.GET["q"]
 
             if search == "month":
-                event = EventOrder.objects.filter(event__user__id=request.user.id)
+
+                event = EventOrder.objects.filter(
+                    event__user__id=request.user.id, created_at__year=today.year
+                )
+
+                event_credit_earning = EventOrder.objects.filter(
+                    event__user__id=request.user.id,
+                    transaction_type="credit",
+                    created_at__year=today.year,
+                )
+                event_direct_earning = EventOrder.objects.filter(
+                    event__user__id=request.user.id,
+                    transaction_type="direct_purchase",
+                    created_at__year=today.year,
+                )
+
+                direct_earning = event_direct_earning.aggregate(Sum("event__price"))
+                credit_earning = event_credit_earning.aggregate(
+                    Sum("event__credit_required")
+                )
+
+                if credit_earning["event__credit_required__sum"] is None:
+                    credit_earning["event__credit_required__sum"] = 0
+
+                if direct_earning["event__price__sum"] is None:
+                    direct_earning["event__price__sum"] = 0
+
+                total = round(
+                    float(credit_earning["event__credit_required__sum"]) * 0.36, 2
+                ) + float(direct_earning["event__price__sum"])
+                data["total_earning"] = total
                 res = monthly_earning(event)
-                total=event.aggregate(Sum('event__price'))
-                data['total_earning'] = total['event__price__sum']
-                data['total_enroll_student'] = event.count()
-                data['earning'] = res[0]
+                data["total_enroll_student"] = event.count()
+                data["earning"] = res[0]
                 result.append(data)
 
                 return Response(
@@ -56,16 +92,35 @@ class InfluencerEarnMoneyListAPIView(MyAPIView):
                     }
                 )
 
-
-
-            elif search == 'year':
+            elif search == "year":
 
                 event = EventOrder.objects.filter(event__user__id=request.user.id)
+                event_credit_earning = EventOrder.objects.filter(
+                    event__user__id=request.user.id, transaction_type="credit"
+                )
+                event_direct_earning = EventOrder.objects.filter(
+                    event__user__id=request.user.id, transaction_type="direct_purchase"
+                )
+
+                direct_earning = event_direct_earning.aggregate(Sum("event__price"))
+                credit_earning = event_credit_earning.aggregate(
+                    Sum("event__credit_required")
+                )
+
+                if credit_earning["event__credit_required__sum"] is None:
+                    credit_earning["event__credit_required__sum"] = 0
+
+                if direct_earning["event__price__sum"] is None:
+                    direct_earning["event__price__sum"] = 0
+
+                total = round(
+                    float(credit_earning["event__credit_required__sum"]) * 0.36, 2
+                ) + float(direct_earning["event__price__sum"])
+                data["total_earning"] = total
+
                 res = yearly_earning(event)
-                total=event.aggregate(Sum('event__price'))
-                data['total_earning'] = total['event__price__sum']
-                data['total_enroll_student'] = event.count()
-                data['earning'] = res
+                data["total_enroll_student"] = event.count()
+                data["earning"] = res
                 result.append(data)
 
                 return Response(
@@ -76,15 +131,44 @@ class InfluencerEarnMoneyListAPIView(MyAPIView):
                     }
                 )
 
-            elif search == 'week':
-                day=7
-                start_date = datetime.now() - timedelta(days = day)
-                event = EventOrder.objects.filter(created_at__range=[start_date, current_datetime], event__user__id=request.user.id)
-                total=event.aggregate(Sum('event__price'))
-                data['total_earning'] = total['event__price__sum']
-                data['total_enroll_student'] = event.count()
-                res=daily_earning(request.user.id, start_date, day)
-                data['earning'] = res
+            elif search == "week":
+                day = 7
+                start_date = datetime.now() - timedelta(days=day)
+                event = EventOrder.objects.filter(
+                    created_at__range=[start_date, current_datetime],
+                    event__user__id=request.user.id,
+                )
+
+                event_credit_earning = EventOrder.objects.filter(
+                    created_at__range=[start_date, current_datetime],
+                    event__user__id=request.user.id,
+                    transaction_type="credit",
+                )
+                event_direct_earning = EventOrder.objects.filter(
+                    created_at__range=[start_date, current_datetime],
+                    event__user__id=request.user.id,
+                    transaction_type="direct_purchase",
+                )
+
+                direct_earning = event_direct_earning.aggregate(Sum("event__price"))
+                credit_earning = event_credit_earning.aggregate(
+                    Sum("event__credit_required")
+                )
+
+                if credit_earning["event__credit_required__sum"] is None:
+                    credit_earning["event__credit_required__sum"] = 0
+
+                if direct_earning["event__price__sum"] is None:
+                    direct_earning["event__price__sum"] = 0
+
+                total = round(
+                    float(credit_earning["event__credit_required__sum"]) * 0.36, 2
+                ) + float(direct_earning["event__price__sum"])
+
+                data["total_earning"] = total
+                data["total_enroll_student"] = event.count()
+                res = daily_earning(request.user.id, start_date, day)
+                data["earning"] = res
                 result.append(data)
 
                 return Response(
@@ -97,9 +181,9 @@ class InfluencerEarnMoneyListAPIView(MyAPIView):
 
         except:
             return Response(
-                    {
-                        "status": "FAIL",
-                        "message": "Bad request",
-                        "data": [],
-                    }
-                )
+                {
+                    "status": "FAIL",
+                    "message": "Bad request",
+                    "data": [],
+                }
+            )
--- core/utils/daily_earning_money.py
+++ core/utils/daily_earning_money.py
@@ -12,14 +12,26 @@ def daily_earning(user, start_date, total_days):
 
     for i in range(0, total_days):
         new_date += timedelta(days=1)
-        event = event = EventOrder.objects.filter(
-            created_at__range=[start_date, new_date], event__user__id=user
+        event_direct_earning  = EventOrder.objects.filter(
+            created_at__range=[start_date, new_date], event__user__id=user, transaction_type="direct_purchase"
         )
-        total = event.aggregate(Sum("event__price"))
+
+        event_credit_earning = EventOrder.objects.filter(
+            created_at__range=[start_date, new_date], event__user__id=user, transaction_type="credit"
+        )
+
+        direct_earning = event_direct_earning.aggregate(Sum("event__price"))
+        credit_earning = event_credit_earning.aggregate(Sum("event__credit_required"))
+        if credit_earning['event__credit_required__sum'] is None:
+            credit_earning['event__credit_required__sum']=0
+
+        if direct_earning['event__price__sum'] is None:
+            direct_earning['event__price__sum']=0
+
+        total = round(float(credit_earning['event__credit_required__sum']) * 0.36, 2) + float(direct_earning['event__price__sum'])
+
         start_date += timedelta(days=1)
-        earning_by_days[str(start_date.strftime("%d-%m-%Y"))] = total[
-            "event__price__sum"
-        ]
+        earning_by_days[str(start_date.strftime("%d-%m-%Y"))] = total
 
     return earning_by_days
 
@@ -28,7 +40,7 @@ def daily_earning(user, start_date, total_days):
 def monthly_earning(event_object):
 
     month_totals = {
-        k: sum(x.event.price for x in g)
+        k: sum(round(x.event.credit_required * 0.36, 2) if x.transaction_type=="credit" else x.event.price for x in g)
         for k, g in groupby(event_object, key=lambda i: i.created_at.month)
     }
 
@@ -59,10 +71,9 @@ def monthly_earning(event_object):
 
 
 def yearly_earning(event_object):
-
+   
     year_totals = {
-        k: sum(x.event.price for x in g)
+        k: sum( round(x.event.credit_required * 0.36, 2) if x.transaction_type=="credit" else x.event.price for x in g)
         for k, g in groupby(event_object, key=lambda i: i.created_at.year)
     }
-
     return year_totals
